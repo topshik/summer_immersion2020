05e33901510e8a33ce7a6d1a580f0b16aad10349
topshik <semenkin.anton@gmail.com>
2020-07-08 15:28:21 +0300

    upd: idx2word

--- utils.py
+++ utils.py
@@ -19,13 +19,15 @@ def to_var(x):
     return x
 
 
-def idx2word(idx, i2w, pad_idx):
+def idx2word(idx, i2w, pad_idx, eos_idx):
     sent_str = [str()]*len(idx)
     for i, sent in enumerate(idx):
         for word_id in sent:
             if word_id == pad_idx:
                 break
             sent_str[i] += i2w[str(word_id.item())] + " "
+            if word_id == eos_idx:
+                break
         sent_str[i] = sent_str[i].strip()
     return sent_str
 
