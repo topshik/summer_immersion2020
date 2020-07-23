34b005fedbad72f008416b0acaa2f92baeb2c0ae
topshik <semenkin.anton@gmail.com>
2020-07-23 18:47:47 +0300

    add: callbacks for checkpointing and early stopping

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from omegaconf import DictConfig
 import pytorch_lightning as pl
 
 import plmodel
+from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 
 
 @hydra.main(config_path="train-config.yaml", strict=False)
@@ -14,24 +15,46 @@ def train(config: DictConfig) -> None:
     os.chdir(hydra.utils.get_original_cwd())
 
     with open(f"{config.hydra_base_dir}/metrics.csv", "w") as output:
-        output.write("epochs number,zero KL epochs number,KL mode,KL beta value,KL,NLL,ELBO,"
+        output.write("version,epochs number,zero KL epochs number,KL mode,KL beta value,KL,NLL,ELBO,"
                      "NLL (importance sampling)\n")
 
-    start_version = max(map(int, [name.split('_')[1] for name in os.listdir("lightning_logs")]))
-    print(f"Starting from version: {start_version + 1}")
+    start_version = max(map(int, [name.split('_')[1] for name in os.listdir("lightning_logs")])) + 1
+    current_version = start_version
+    print(f"Starting from version: {start_version}")
     print(f"Writing logs to file {config.hydra_base_dir}/metrics.csv")
-    eps = 1e-4
 
-    for beta in np.linspace(0 + eps, 20, 8):
-        for mode in ["const", "linear"]:
-            config.kl_weight = beta
-            config.anneal_function = mode
+    beta_space = np.linspace(1e-2, 5, 10)
+    modes_space = ["const", "linear", "logistic"]
+
+    for beta in beta_space:
+        for mode in modes_space:
+            config.kl.weight = beta
+            config.kl.anneal_function = mode
+
+            with open(f"{config.hydra_base_dir}/metrics.csv", "a") as output:
+                output.write(f"{current_version},")
+
             model = plmodel.PLSentenceVAE(config)
-            trainer = pl.Trainer(max_epochs=config.max_epochs, gpus=1, auto_select_gpus=True)
+            checkpoint_callback = ModelCheckpoint(
+                save_top_k=(config.chkpnt.top_k if config.chkpnt.top_k != -1 else config.train.max_epochs),
+                verbose=True,
+                monitor="val_loss",
+                mode="min",
+                prefix=f"version_{current_version}_"
+            )
+            trainer = pl.Trainer(max_epochs=config.train.max_epochs,
+                                 gpus=1,
+                                 auto_select_gpus=True,
+                                 early_stop_callback=plmodel.ValLossEarlyStopping(patience=3, min_delta=0.3),
+                                 checkpoint_callback=checkpoint_callback)
             trainer.fit(model)
 
-    with open(f"{config.hydra_base_dir}/metrics.csv", "w") as output:
-        output.write(f"Started from version: {start_version + 1}")
+            current_version += 1
+
+    with open(f"{config.hydra_base_dir}/metrics.csv", "a") as output:
+        output.write(f"Started from version: {start_version}")
+
+    print(f"Check metrics in file {config.hydra_base_dir}/metrics.csv")
 
 
 if __name__ == "__main__":
