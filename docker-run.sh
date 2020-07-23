a0eff4f1ee77198ef83d0f761edeb87e2ae60345
topshik <semenkin.anton@gmail.com>
2020-07-23 18:47:08 +0300

    upd: multiple gpus

--- docker-run.sh
+++ docker-run.sh
@@ -2,10 +2,9 @@ docker run \
 	-it \
 	--memory=64g \
 	--memory-swap=64g \
-	--cpuset-cpus=0-9 \
-	--gpus device=2 \
+	--cpuset-cpus=0-19 \
+	--gpus '"device=2,3"' \
 	--volume ~/summer_immersion2020:/home/user/immersion \
 	--workdir /home/user/immersion \
 	-p 1337:6006 \
 	semenkin-immersion
-
