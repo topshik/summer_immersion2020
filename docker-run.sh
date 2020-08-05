38deb7f35d20faf31f0b561dc0ff0b8c25c14c37
topshik <semenkin.anton@gmail.com>
2020-08-05 19:44:43 +0300

    upd: working directory

--- docker-run.sh
+++ docker-run.sh
@@ -4,7 +4,7 @@ docker run \
 	--memory-swap=64g \
 	--cpuset-cpus=0-19 \
 	--gpus '"device=2,3"' \
-	--volume ~/summer_immersion2020:/home/user/immersion \
-	--workdir /home/user/immersion \
+	--volume ~/summer_immersion2020:/home/user/summer_immersion2020 \
+	--workdir /home/user/summer_immersion2020 \
 	-p 1337:6006 \
 	semenkin-immersion
